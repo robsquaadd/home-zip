baf68de1be1707ea4057663cd456211ad223af81
robsquaadd <102495137+robsquaadd@users.noreply.github.com>
2022-05-25 23:58:59 -0400

    changed from real estate to seatgeek api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,13 @@
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Host": "realty-in-us.p.rapidapi.com",
-    "X-RapidAPI-Key": "d84de6e21amsha8ee7921306ee3fp1316c8jsne853c68ba923",
-  },
-};
-
-var seatGeekURL = "https://api.seatgeek.com/2/events";
+var clientID = "MjIzNjI4MTd8MTY1MzUyMjkzMS42NzQ0ODU";
+var clientSecret =
+  "b340483a7a27e88d5f194b3ecaff1d876de82fb6f8ad348d9ea12020a1b129cf";
+var seatGeekURL =
+  "https://api.seatgeek.com/2/events?client_id=MjIzNjI4MTd8MTY1MzUyMjkzMS42NzQ0ODU";
 var googleMapsURL = "";
 
-const retrieveZillow = async () => {
+const retrieveEvents = async () => {
   try {
-    const response = await fetch(zillowURL, options);
+    const response = await fetch(seatGeekURL);
     const data = await response.json();
     console.log(data);
   } catch (error) {
@@ -19,7 +15,7 @@ const retrieveZillow = async () => {
   }
 };
 
-retrieveZillow();
+retrieveEvents();
 
 // clientSecret b340483a7a27e88d5f194b3ecaff1d876de82fb6f8ad348d9ea12020a1b129cf
 // clientID MjIzNjI4MTd8MTY1MzUyMjkzMS42NzQ0ODU
