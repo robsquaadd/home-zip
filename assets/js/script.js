6e6aa3a1bba6dd4b3aecbdc566d6c7c67bf360cd
robsquaadd <102495137+robsquaadd@users.noreply.github.com>
2022-05-30 20:20:15 -0400

    seatgeek api reinserted

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,5 @@
-var seatGeekURL = "https://api.seatgeek.com/2/events";
-var googleMapsURL = "";
-var googleMapsAPIKey = "AIzaSyD7z6JSswDDXqG0HeApZcOasb4Pm0_JVso";
-
-const retrieveEvents = async () => {
-  try {
-    const response = await fetch(seatGeekURL);
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-retrieveZillow();
-
 //clientSecret b340483a7a27e88d5f194b3ecaff1d876de82fb6f8ad348d9ea12020a1b129cf
 // clientID MjIzNjI4MTd8MTY1MzUyMjkzMS42NzQ0ODU
-
-// <input type = 'submit' placeholder = 'Location'></input>
-// add an event listener on the input field that saves the user's choice
-// loop through events api for events in user inputted location.
-// dynamically generate cards to display on the homepage.
-// dynamically populate the modal with event information.
 
 //Grabbing the flip card class from HTML
 let filpCard = document.querySelector(".flip");
@@ -35,23 +13,22 @@ let searchInput = document.querySelector("#search");
 
 function search() {
   filpCard.classList.remove("hide");
-
   console.log(searchInput.value);
   apiTM();
 }
 
 function apiTM() {
-  let apiKey = "hhfyWLPUTrRWrh5U9TWOti5vcnswA9gG";
-  let apiUrl =
-    " https://app.ticketmaster.com/discovery/v2/events.json?" +
-    searchInput.value +
-    "=US&apikey=hhfyWLPUTrRWrh5U9TWOti5vcnswA9gG";
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      console.log(apiUrl);
-      return response.json();
-    } else {
-      alert(" Error Try Again");
-    }
-  });
+  let apiKey = "MjIzNjI4MTd8MTY1MzUyMjkzMS42NzQ0ODU";
+  let apiUrl = `https://api.seatgeek.com/2/events?client_id=${apiKey}`;
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert(" Error Try Again");
+      }
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 }
